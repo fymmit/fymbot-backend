3f5f84cc568a50b0fd1dd3582b6103cb107c8bb0
fymmit <blomqvist.timo@gmail.com>
2018-06-02 23:13:32 +0300

    Add /POST for adding users and giving/taking points.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = require('express')();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const secret = process.env.SECRET;
+
 app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -13,14 +15,48 @@ app.use(bodyParser.json());
 
 var users = [
     {
-        name: "Heikki", points: 100
+        id: '1',
+        name: 'Heikki',
+        points: 100
     },
     {
-        name: "Markku", points: 200
+        id: '2',
+        name: 'Markku',
+        points: 200
     }
 ]
 
 app.get('/users', (req, res) => {
+    res.json(users);
+})
+
+app.post(`/${secret}/users`, (req, res) => {
+    var user = {
+        id: '',
+        name: '',
+        points: 0
+    }
+    user.id = (users.length + 1).toString();
+    user.name = req.body.name;
+    users.push(user);
+    console.log(user);
+    res.json(users);
+})
+
+app.post(`/${secret}/points`, (req, res) => {
+    var userFound = false;
+    for (var i = 0; i < users.length; i++) {
+        var user = users[i];
+        if (user.name == req.body.name) {
+            user.points += req.body.points;
+            userFound = true;
+            break;
+        }
+    }
+    if (!userFound) {
+        res.send('User not found.');
+    }
+    console.log(users);
     res.json(users);
 })
 
